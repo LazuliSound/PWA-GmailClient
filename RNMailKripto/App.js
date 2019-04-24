1b72da6e3e0fc734ec2fe8ba126006493c660a21
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2019-04-24 16:16:25 +0700

    fix minor changes

--- RNMailKripto/App.js
+++ RNMailKripto/App.js
@@ -1,10 +1,65 @@
 import React from 'react';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { Appbar, TextInput, Button, List, TouchableRipple, Portal, Dialog, Provider } from 'react-native-paper';
 import Dimensions from 'Dimensions';
 const {deviceWidth, deviceHeight} = Dimensions.get('window');
 
-export default class App extends React.Component {
+class HomeScreen extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',      
+    }
+  } 
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <Appbar.Header>
+          <Appbar.Content
+            title="Mail Kripto"
+            subtitle="Konfigurasi akun surel"
+          />
+        </Appbar.Header>
+        <TextInput
+          type="email-address"
+          style={styles.textInput}
+          placeholder="Email"
+          onChangeText={(email) => this.setState({email})}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Password"
+          secureTextEntry={true}
+          onChangeText={(password) => this.setState({password})}
+        />
+        <Button mode="contained" onPress={() => this.props.navigation.navigate('Details')}>
+          To Details
+        </Button>
+
+      </ScrollView>
+    );
+  }
+}
+
+
+class ProfileScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {  
+    return (
+      <View>
+        <Text>{this.props.name}</Text>
+      </View>
+    );
+  }
+}
+
+class InboxScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -66,7 +121,7 @@ export default class App extends React.Component {
           <Appbar.Header>
             <Appbar.Content
               title="Mail Kripto"
-              subtitle="Menggunakan digital signature"
+              subtitle="Inbox"
             />
           </Appbar.Header>
           <View style={styles.controller}>
@@ -157,6 +212,37 @@ export default class App extends React.Component {
         </Portal>
       </Provider>
     );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+        <Button mode="contained" onPress={() => this.props.navigation.navigate('Home')}>
+          Go to Home Screen
+        </Button>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
   }
 }
 
